22be27efc475995df4265b09ffdeb73d65b406ec
Christopher G Gonzalez <christopherggonzalez@Christophers-MacBook-Air.local>
2024-02-08 20:24:49 -0800

    activity 3

---
+++ day9/activites/Activity3.js
@@ -0,0 +1,12 @@
+//Create a function expression named add that takes two numbers as parameters and returns their sum. Call the function to add 5 and 7, then log the result./
+
+const add = function (number1,number2) { 
+    return number1 + number2;
+};
+
+//adding 5 and 7
+
+const result = add(5,7);
+console.log(`Total sum (5 + 7 = ${result})`);
+//console.log('Total sum (5 + 7  = )${result}');
+//function add (numbers1,number2)
