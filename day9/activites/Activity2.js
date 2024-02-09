a2a42f8edc95e82a78d8c55ab66420382894e70d
Christopher G Gonzalez <christopherggonzalez@Christophers-MacBook-Air.local>
2024-02-08 20:16:53 -0800

    activity 2

---
+++ day9/activites/Activity2.js
@@ -0,0 +1,9 @@
+//Create a function called greet that takes a name as a parameter and logs a greeting message with the name to the console. Then, call the function with your name./
+
+const hello = prompt("Enter your name :")
+function greetings (names) { 
+    console.log(`Hello greetings , ${names}!`)
+}
+
+
+greetings(hello);
